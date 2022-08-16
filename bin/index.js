af617c374412a28245af79fe631ae2cfafae952f
Italo Castro <dev.castro.italo@gmail.com>
2022-08-15 21:19:10 -0300

    initialize git repository

--- bin/index.js
+++ bin/index.js
@@ -1,6 +1,17 @@
 #! /usr/bin/env node
 
 import { generate } from './utils.js';
+import { exec } from 'node:child_process';
 
-generate();
+await generate();  // Generates folders and files for a MVC project
+
+// Initialize a git repository
+exec('git init', (err, stdout, stderr) => {  
+    if (err) {
+        throw new Error(err);
+    }
+
+    console.log(stdout);
+    console.log(stderr);
+});
 
